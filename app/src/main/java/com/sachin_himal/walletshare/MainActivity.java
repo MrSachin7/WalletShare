ebf81dac14d0082b8f2fda07a88982ae9894d16c
Himal sharma <41054866+himal28924@users.noreply.github.com>
2022-11-08 21:36:41 +0100

    Change of button nav and add button open new activity

--- app/src/main/java/com/sachin_himal/walletshare/MainActivity.java
+++ app/src/main/java/com/sachin_himal/walletshare/MainActivity.java
@@ -5,19 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 import com.sachin_himal.walletshare.view.Fragments.HomeFragment;
 import com.sachin_himal.walletshare.view.Fragments.NewTranscationFragment;
 import com.sachin_himal.walletshare.view.Fragments.ProfileFragment;
+import com.sachin_himal.walletshare.view.addExpenditure.AddExpenditure;
 
 public class MainActivity extends AppCompatActivity  implements NavigationBarView.OnItemSelectedListener {
 
-     BottomNavigationView bottomNavigationView;
+
+    BottomNavigationView bottomNavigationView;
     private HomeFragment homeFragment ;
     private NewTranscationFragment newTranscationFragment;
     private ProfileFragment profileFragment;
@@ -30,13 +35,13 @@ public class MainActivity extends AppCompatActivity  implements NavigationBarVie
         setContentView(R.layout.activity_main);
 
 
-
         //Initializing fragment and setting up
         setUpFragments();
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationBarView);
 
 
+        bottomNavigationView.setBackground(null);
 
         bottomNavigationView.setOnItemSelectedListener(this);
 
@@ -44,8 +49,37 @@ public class MainActivity extends AppCompatActivity  implements NavigationBarVie
         bottomNavigationView.setSelectedItemId(R.id.menu_dashboard);
 
 
+        /**
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            System.out.println("FUCCKKCK");
+            switch(id){
+                case R.id.menu_dashboard:
+                    System.out.println("FUCCKKCK Dashboard");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                    return true;
+                case R.id.menu_addTransaction:
+                    System.out.println("FUCCKKCK transcation");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, newTranscationFragment).commit();
+                    return true;
+
+                case R.id.menu_profile:
+                    System.out.println("FUCCKKCK profile");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                    return true;
+            }
+            return true;
+        });
+         **/
 
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddExpenditure.class));
+            }
+        });
     }
 
 
@@ -61,8 +95,6 @@ public class MainActivity extends AppCompatActivity  implements NavigationBarVie
         homeFragment = new HomeFragment();
         newTranscationFragment = new NewTranscationFragment();
         profileFragment = new ProfileFragment();
-
-
 
     }
 
