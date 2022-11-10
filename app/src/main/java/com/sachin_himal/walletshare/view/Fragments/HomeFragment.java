e981b51b04f9f9f6952b22c43d57249ff33dad46
Himal sharma <41054866+himal28924@users.noreply.github.com>
2022-11-10 22:25:43 +0100

    Working with Card view it doesn't look good have to think about design again

--- app/src/main/java/com/sachin_himal/walletshare/view/Fragments/HomeFragment.java
+++ app/src/main/java/com/sachin_himal/walletshare/view/Fragments/HomeFragment.java
@@ -63,4 +63,8 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    public void seeMoreRecords(View view) {
+
+    }
 }
